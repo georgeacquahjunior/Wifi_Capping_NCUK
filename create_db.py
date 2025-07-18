130069692d8f83a8d8009903fbe66a6e31ffba77
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2025-07-18 03:23:42 +0000

    sample code to create db from models

---
+++ create_db.py
@@ -0,0 +1,15 @@
+# backend/create_db.py
+
+from app import app
+from models.student import db, Student
+
+with app.app_context():
+    db.create_all()
+
+    if not Student.query.filter_by(student_id="student001").first():
+        student = Student(student_id="student001", name="George Acquah")
+        student.set_password("test122")
+        db.session.add(student)
+        db.session.commit()
+
+    print("Database and tables created.")
