f0c4065bf2fda416c4228db7fa88dbbe48a63703
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2025-08-18 22:41:26 +0000

    feat: update app.py to set dynamic port configuration for deployment

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from models.student_db import db, Student
 from models.admin_db import db, Admin
 from routes.students_routes import student_bp
 from routes.admin_routes import admin_bp
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,4 +19,5 @@ app.register_blueprint(admin_bp)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env var
+    app.run(host="0.0.0.0", port=port)
