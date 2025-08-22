eb6c081d505deebf5e1714d215e50e5ba765535f
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2025-08-22 09:56:01 +0000

    updated code

--- create_db.py
+++ create_db.py
@@ -2,14 +2,12 @@
 
 from app import app
 from models.student_db import db, Student
+from models.merged_usage_log_db import db, MergedUsageLogs
+from models.admin_db import db, Admin
 
 with app.app_context():
     db.create_all()
 
-    if not Student.query.filter_by(student_id="student001").first():
-        student = Student(student_id="student001", name="George Acquah")
-        student.set_password("test122")
-        db.session.add(student)
-        db.session.commit()
+
 
     print("Database and tables created.")
